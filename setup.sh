df828f615e4b148ee6656c1eabbc088343c60ab5
Le Quang Hieu <hieulw99@gmail.com>
2020-10-21 14:02:20 +0700

    :bug:

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-# if [[ $SUDO_USER ]]; then
-#   echo "Try again without sudo"
-#   exit 1
-# fi
+if [[ $SUDO_USER ]]; then
+  echo "Try again without sudo"
+  exit 1
+fi
 
-# sudo pacman -S --needed git base-devel fish tmux vifm neovim
+sudo pacman -S --needed git base-devel fish tmux vifm neovim
 
-# # prompt starship
-# echo "Install Starship Prompt"
-# curl -fsSL https://starship.rs/install.sh | bash
+# prompt starship
+echo "Install Starship Prompt"
+curl -fsSL https://starship.rs/install.sh | bash
 
-# # tmux plugins
-# echo "Install Tmux Plugins"
-# git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+# tmux plugins
+echo "Install Tmux Plugins"
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
-# # neovim
-# echo "Install Vim-Plug"
-# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
-#       --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# neovim
+echo "Install Vim-Plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+      --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# # python
-# echo "Install pyenv"
-# curl https://pyenv.run | bash
-# git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git \
-#   $(pyenv root)/plugins/pyenv-virtualenvwrapper
+# python
+echo "Install pyenv"
+curl https://pyenv.run | bash
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git \
+  $(pyenv root)/plugins/pyenv-virtualenvwrapper
 
-# # node
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
-# nvm install --lts
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+nvm install --lts
 
 dotfiles=$(ls -1A --ignore={setup.sh,README.md,.git,.local})
 
